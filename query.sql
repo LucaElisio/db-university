67c16ddb0c6fd99f967b0dcc49b054d8222ec8e0
LucaElisio <personal.elix@gmail.com>
2024-06-01 18:36:56 +0200

    feat: group by and join excercise

--- query.sql
+++ query.sql
@@ -49,9 +49,93 @@ VALUES (1, "Luca", "Elisio", "1999-08-29", "LSELCU99M29A345G", "2024-01-01", 010
 -- 10. Cambiare il numero dell’ufficio del professor Pietro Rizzo in 126
 UPDATE `teachers`
 SET `office_number` = 126
-WHERE `name` = "Pietro"
--- AND "surname" = "Rizzo" Non funziona così
+WHERE `teachers`.`name` = "Pietro"
+AND `teachers`.`surname` = "Rizzo"
+
+
 
 -- 11. Eliminare dalla tabella studenti il record creato precedentemente al punto 9
 DELETE FROM `students`
 WHERE `surname` = "Elisio";
+
+
+------------------------------------------------------------------------
+-- GROUP BY EXCERCISE
+
+-- 1. Contare quanti iscritti ci sono stati ogni anno
+
+SELECT YEAR(`students`.`enrolment_date`), COUNT(*) AS `num_of_students`
+FROM `students`
+GROUP BY YEAR(`students`.`enrolment_date`)
+
+-- 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
+
+SELECT `teachers`.`office_address`, COUNT(*) AS `same_office`
+FROM `teachers`
+GROUP BY `teachers`.`office_address`
+
+-- 3. Calcolare la media dei voti di ogni appello d'esame
+
+SELECT `exam_student`.`exam_id`, AVG(`exam_student`.`vote`) AS `average_vote` 
+FROM `exam_student` 
+GROUP BY `exam_student`.`exam_id`;
+
+-- 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
+
+SELECT `degrees`.`department_id` ,`degrees`.`name`, COUNT(*) AS `num_of_courses`
+FROM `degrees` 
+GROUP BY `degrees`.`department_id`
+
+
+
+---------------------------------------------------------------------------
+-- JOIN EXCERCISE
+
+-- 1. Selezionare tutti gli studenti iscritti al Corso di Laurea in Economia
+
+SELECT `students`.`id`, `students`.`name`, `students`.`surname`      
+FROM `students`
+INNER JOIN `degrees`
+ON `students`.`degree_id` = `degrees`.`id`
+WHERE `degrees`.`name` = "Corso di Laurea in Economia";
+
+-- 2. Selezionare tutti i Corsi di Laurea Magistrale del Dipartimento di Neuroscienze
+
+SELECT *
+FROM `departments`
+INNER JOIN `degrees`
+ON `departments`.`id` = `degrees`.`department_id`
+WHERE `degrees`.`level`="magistrale"
+AND `departments`.`name` = "dipartimento di neuroscienze";
+
+-- 3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
+
+SELECT *
+FROM `teachers`
+INNER JOIN `course_teacher`
+ON `course_teacher`.`teacher_id` = `teacher`.`id`
+INNER JOIN `courses`
+ON `courses_teacher`.`course_id` = `course`.`id`
+WHERE `teachers`.`id` = "44"
+
+-- 4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome
+
+SELECT 	`students`.`surname` AS `Cognome`, `students`.`name` AS `Nome`, `degrees`.`name` AS `Corso di laurea`, `departments`.`name` AS `Dipartimento`
+FROM `students`
+INNER JOIN `degrees`
+ON `students`.`degree_id` = `degrees`.`id`
+INNER JOIN `departments`
+ON `departments`.`id` = `degrees`.`department_id`
+ORDER BY `students`.`surname`, `students`.`name`;
+
+-- 5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
+
+SELECT 	`teachers`.`name` AS `Nome`, `teachers`.`surname` As `Cognome`, `degrees`.`name` AS `Corso laurea`, `courses`.`name` AS `Corso`
+FROM `teachers`
+INNER JOIN `course_teacher`
+ON `teachers`.`id` = `course_teacher`.`teacher_id`
+INNER JOIN `courses`
+ON `course_teacher`.`course_id` = `courses`.`id`
+INNER JOIN `degrees`
+ON `courses`.`degree_id` = `degrees`.`id`
+
